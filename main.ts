93e529a8071e34cca067de67625327f47266aa6b
H4ZARD9 <122539729+H4ZARD9@users.noreply.github.com>
2023-01-26 08:46:04 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    dot += 1
+    basic.showString("" + (dot))
+})
+input.onButtonPressed(Button.AB, function () {
+    dot = 0
+    basic.showString("" + (dot))
+})
+input.onButtonPressed(Button.B, function () {
+    dot += -1
+    basic.showString("" + (dot))
+})
+let dot = 0
+dot = 0
+basic.showString("" + (dot))
+basic.forever(function () {
+	
+})
